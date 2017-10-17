1278f1ed7aaf906d2b017ea9fe676a41fe676e91
Arjen van der Pas <a.vanderpas@bkr.nl>
2017-10-17 15:48:46 +0200

    method result toegevoegd

--- HelloWorldClass/HelloWorldClass/Hi.cs
+++ HelloWorldClass/HelloWorldClass/Hi.cs
@@ -8,5 +8,12 @@ namespace HelloWorldClass
 {
     public class Hi
     {
+        public string result(string input)
+        {
+            if (input == "HELLO")
+                return "WORLD";
+            return "INVALID";
+        }
+
     }
 }
